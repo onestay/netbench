36919d5dfbceb8ad63b987239cf3ec8d03736786
Marius Meschter <git@onestay.moe>
2022-09-27 22:41:30 +0200

    test sending of the NewTestMessage

--- tests/server_test.rs
+++ tests/server_test.rs
@@ -1,13 +1,15 @@
-use netbench::Client;
-use netbench::{ClientConfig, Server, ServerConfig};
-#[tokio::test]
-async fn test_listen() {
-    let mut s = Server::new(ServerConfig {});
-    s.listen().await.unwrap();
-}
+use netbench::{Client, ClientConfig, Server, ServerConfig};
+use tokio::time::{sleep, Duration};
 
 #[tokio::test]
-async fn test_connect() {
-    let c = Client::new(ClientConfig {});
-    c.connect().await.unwrap();
+async fn test_start_new_test() {
+    let (mut server, ch) = Server::new(ServerConfig { addr: "127.0.0.1:8080".parse().unwrap() }).await.unwrap();
+    let handle = tokio::spawn(async move {
+        server.accept().await.unwrap();
+    });
+    let client = Client::new(ClientConfig {addr: "127.0.0.1:8080".parse().unwrap()}).await.unwrap();
+    client.start_new_test().await.unwrap();
+    sleep(Duration::from_secs(1)).await;
+    ch.send(netbench::ControlMessage::Stop).await.unwrap();
+    handle.await.unwrap();
 }
